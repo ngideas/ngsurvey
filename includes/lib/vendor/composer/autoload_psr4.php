878d711b4568ff0975fc5d99684f46ae656b9234
NgIdeas <support@ngideas.com>
2021-10-30 11:18:03 +0530

    Update action scheduler package files

--- includes/lib/vendor/composer/autoload_psr4.php
+++ includes/lib/vendor/composer/autoload_psr4.php
@@ -8,7 +8,7 @@ $baseDir = dirname($vendorDir);
 return array(
     'Symfony\\Polyfill\\Php80\\' => array($vendorDir . '/symfony/polyfill-php80'),
     'Symfony\\Component\\CssSelector\\' => array($vendorDir . '/symfony/css-selector'),
-    'Psr\\Log\\' => array($vendorDir . '/psr/log/src'),
+    'Psr\\Log\\' => array($vendorDir . '/psr/log/Psr/Log'),
     'Pelago\\Emogrifier\\' => array($vendorDir . '/pelago/emogrifier/src'),
     'Monolog\\' => array($vendorDir . '/monolog/monolog/src/Monolog'),
     'DeviceDetector\\' => array($vendorDir . '/matomo/device-detector'),
